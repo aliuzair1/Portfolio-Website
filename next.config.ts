2514ca9ad2746ed93f21f28d871999e5b5651918
Ali Uzair <161635572+aliuzair1@users.noreply.github.com>
2026-06-16 18:16:44 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  basePath: "/Portfolio-Website",
+  assetPrefix: "/Portfolio-Website",
   transpilePackages: ["three"],
   turbopack: {},
 };
